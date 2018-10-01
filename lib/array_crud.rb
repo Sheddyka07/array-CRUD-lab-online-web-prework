8fb4e6bdc6afb8ce82317dc7d2caa79f2f82277d
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 12:50:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  pets = ["dog", "cat", "rabbit", "pig"]
+  pets = ["dog", "cat", "rabbit", "arrays"]
   pets.pop
 end
 
