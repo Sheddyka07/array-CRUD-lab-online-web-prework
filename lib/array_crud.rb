bf2e80fe344e80a1c18c732f10123402137c4f8f
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 12:49:41 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   pets = ["dog", "cat", "rabbit", "pig"]
-  pets.unshift("first!")
+  pets.pop("")
 end
 
 def remove_element_from_start_of_array(array)
