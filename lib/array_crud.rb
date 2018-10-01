dbcb954a43fb7b552eacc30b7e11d0b4634bbd5f
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 12:48:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   pets = ["dog", "cat", "rabbit", "pig"]
-  pets.push("frog")
+  pets.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
